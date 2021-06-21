3b0dceed5e949a3fbf64d89be93f175982270d0b
Deepika <75814956+deepika-beep@users.noreply.github.com>
2021-06-21 23:06:07 +0200

    addedparcel build tool

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-class MyFlixApplication extends React.component{
+class MyFlixApplication extends React.Component{
   render(){
     return (
       <div className='my-flix'>
