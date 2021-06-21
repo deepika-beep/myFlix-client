8c5095681a4ca7424a5417cadb7e341f8aa0655b
Deepika <75814956+deepika-beep@users.noreply.github.com>
2021-06-21 13:12:06 +0200

    added parcel build tool

---
+++ src/README.md
@@ -0,0 +1,11 @@
+ React App (myFlix)
+
+ Objective:
+
+Using React, build the client-side for an application called myFlix based on its existing
+server-side code (REST API and database).
+
+User Stories
+● As a user, I want to be able to access information on movies, directors, and genres so
+that I can learn more about movies I’ve watched or am interested in.
+
---
+++ src/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+class MyFlixApplication extends React.component{
+  render(){
+    return (
+      <div className='my-flix'>
+        <div>good morning</div>
+        </div>
+    );
+  }
+}
+// Finds the root of your app
+const container = document.getElementsByClassName('app-container')[0];
+
+// Tells React to render your app in the root DOM element
+ReactDOM.render(React.createElement(MyFlixApplication), container);
