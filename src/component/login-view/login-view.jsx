f6ecc4f4cf70d3a117cca5df0fe89494cf65799f
Deepika <75814956+deepika-beep@users.noreply.github.com>
2021-06-29 15:06:01 +0200

    added toggle form

--- src/component/login-view/login-view.jsx
+++ src/component/login-view/login-view.jsx
@@ -2,23 +2,27 @@
 import React,{useState} from 'react';
 export function LoginView(props){
   // useState() method is initial value of login variable,returns an array assigning current value to username,setUsername  variable a method to  update username variable.
-  const[username,setUsername]=useState('');
-  const[password,setPassword]=useState('');
-}
-  
+  const[username,setUsername] = useState('');
+  const[password,setPassword] = useState('');
+
 // handleSubmit will log username and password into console
-const handleSubmit = () =>{
+const handleSubmit = (e) =>{
   e.preventDefault();
   console.log(username,password);
-  // props.onLoggedIn(username); allow users to be automatically logged in
+   // allow users to be automatically logged in
+   props.onLoggedIn(username); 
+ 
 }
  return(
    <form>
      <label>Username:
        <input type = 'text' value = {username} onChange = {e => setUsername(e.target.value)}/>
        </label>
-       <label>Password:</label>
+       <label>Password:
        <input type = "text" value = {password} onChange = {e => setPassword(e.target.value)} />
+       </label>
        <button type='submit' onClick={handleSubmit}>Submit</button>
+       <button type='button' onClick={props.toggleForms}>if no account register</button>
        </form>
  )
+}
