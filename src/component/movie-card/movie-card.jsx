fb4589edc15268e499c68f9fed47ef55dc877032
Deepika <75814956+deepika-beep@users.noreply.github.com>
2021-06-30 16:20:20 +0200

    Update movie-card.jsx

--- src/component/movie-card/movie-card.jsx
+++ src/component/movie-card/movie-card.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 // propTypes avoid bugs and validates datatypes
 import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 // create Moviecard component
 export class MovieCard extends React.Component{
   render(){
     // 2 props-one function(onMovieclick) and object (movieData)
     const {movieData,onMovieClick} = this.props;
-   return <div className='movie-card' onClick={()=>{onMovieClick(movieData);}}>{movieData.Title}</div>
+   return (
+   <Card>
+   <Card.Img variant="top" src={movieData.ImagePath} />
+  <Card.Body>
+    <Card.Title>{movieData.Title}</Card.Title>
+    <Card.Text>{movieData.Description}</Card.Text>
+    <Button variant="link" onClick={()=>{onMovieClick(movieData)}}>Open</Button>
+  </Card.Body>
+   </Card>
+   );
   }
 }
 // set static propTypes property on MovieCard to an object that contains special values
@@ -17,8 +28,16 @@ movieData:PropTypes.shape({
   Title:PropTypes.string.isRequired,
   Description:PropTypes.string.isRequired,
   ImagePath:PropTypes.string.isRequired,
-  Genre:PropTypes.string.isRequired,
-  Director:PropTypes.string.isRequired
+  Genre:PropTypes.shape({
+    Name: PropTypes.string.isRequired,
+      Description: PropTypes.string.isRequired
+  }),
+  Director:PropTypes.shape({
+    Name: PropTypes.string.isRequired,
+      Bio: PropTypes.string.isRequired,
+      Birth: PropTypes.string.isRequired,
+      Death:PropTypes.string.isRequired
+  }),
 }).isRequired,
 onMovieClick:PropTypes.func.isRequired
 };
