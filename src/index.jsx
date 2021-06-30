61fbf314431f1584ea4f65c643f58655fd444754
Deepika <75814956+deepika-beep@users.noreply.github.com>
2021-06-30 16:19:52 +0200

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Container from 'react-bootstrap/Container';
 // To import main-view into other index-jsx file
 import MainView from './component/main-view/main-view';
 // Import statement to indicate that you need to bundle `./index.scss
@@ -7,7 +8,9 @@ import './index.scss';
 class MyFlixApplication extends React.Component{
   render(){
     return (
+      <Container>
       <MainView/>
+      </Container>
     );
   }
 }
