6698f39da2ebfdbf5f754eb9e2ada2d83868b70d
Deepika <75814956+deepika-beep@users.noreply.github.com>
2021-07-01 15:58:42 +0200

    Update login-view.jsx

--- src/component/login-view/login-view.jsx
+++ src/component/login-view/login-view.jsx
@@ -4,6 +4,7 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Container  from 'react-bootstrap/Container';
 export function LoginView(props){
   // useState() method is initial value of login variable,returns an array assigning current value to username,setUsername  variable a method to  update username variable.
   const[username,setUsername] = useState('');
@@ -20,24 +21,19 @@ const handleSubmit = (e) =>{
  return(
    <Container className="login-view" fluid="true">
    <Form>
-     <Form.Group controlid='formUsername'>
+     <Form.Group controlId='formUsername'>
      <Form.Label>Username:</Form.Label>
        <Form.Control type = 'text' value = {username} onChange = {e => setUsername(e.target.value)}/>
          </Form.Group>
-      <Form.Group controlID='formPassword'>
+      <Form.Group controlId='formPassword'>
        <Form.Label>Password:</Form.Label>
        <Form.Control type = "text" value = {password} onChange = {e => setPassword(e.target.value)} />
        </Form.Group>
+      
        <button variant='primary' type='submit' onClick={handleSubmit}>Submit</button>
        <button variant='primary' type='button' onClick={props.toggleForms}>Register</button>
+       
        </Form>
        </Container>
  )
-}
-LoginView.propTypes = {
-  user: PropTypes.shape ({
-    username: PropTypes.string.isRequired,
-    password: PropTypes.string.isRequired
-  }),
-   onLoggedIn: PropTypes.func.isRequired
 }
