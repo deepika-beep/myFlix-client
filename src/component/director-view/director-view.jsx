7510d6db5316aa6564a9dc852ae1a52af2a28dcd
Deepika <75814956+deepika-beep@users.noreply.github.com>
2021-07-01 11:13:03 +0200

    updated director view

---
+++ src/component/director-view/director-view.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Jumbotron } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import PropTypes from 'prop-types';
+import './director-view.scss';
+
+export class DirectorView extends React.Component {
+
+  render() {
+    const { director, onBackClick } = this.props;
+
+    return (
+      <Jumbotron fluid className="Director-View">
+        <div className="director-view">
+          <div className="director-name">
+            <span className="label">Director: </span>
+            <span className="value">{director.Name}</span>
+          </div>
+          <div className="director-bio">
+            <span className="label">Bio: </span>
+            <span className="value">{director.Bio}</span>
+          </div>
+          <div className="director-birth">
+            <span className="label">DOB: </span>
+            <span className="value">{director.Birth}</span>
+          </div>
+          <div className="director-death">
+            <span className="label">DOD: </span>
+            <span className="value">{director.Death}</span>
+          </div>
+          <Button variant="link" onClick={() => { onBackClick(null); }}>Back</Button>
+        </div>
+      </Jumbotron>
+    );
+  }
+}
+
+DirectorView.propTypes = {
+  director: PropTypes.shape({
+    Name: PropTypes.string.isRequired,
+    Bio: PropTypes.string.isRequired,
+    Birth: PropTypes.string.isRequired,
+     Death: PropTypes.string.isRequired
+  }),
+  onBackClick: PropTypes.func.isRequired
+};
