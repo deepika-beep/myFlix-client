cef9f2c3ea1753cbe99a898984a230547f5e71d1
Deepika <75814956+deepika-beep@users.noreply.github.com>
2021-07-02 11:34:39 +0200

    Update login-view.jsx

--- src/component/login-view/login-view.jsx
+++ src/component/login-view/login-view.jsx
@@ -5,19 +5,32 @@ import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Container  from 'react-bootstrap/Container';
+// When a user clicks on the submit button, you need to update your handleSubmit method, where you’ll make a POST request to the login endpoint using Axio
+import axios from 'axios';
 export function LoginView(props){
   // useState() method is initial value of login variable,returns an array assigning current value to username,setUsername  variable a method to  update username variable.
+  
   const[username,setUsername] = useState('');
   const[password,setPassword] = useState('');
 
 // handleSubmit will log username and password into console
 const handleSubmit = (e) =>{
   e.preventDefault();
-  console.log(username,password);
-   // allow users to be automatically logged in
-   props.onLoggedIn(username); 
+  // send request to server for authentication
+  axios.post('https://myflix-movies-api.herokuapp.com/login',{
+    Username:username,
+    Password:password
+  }).then(response =>{
+    const data = response.data;
+    props.onLoggedIn(data);
+  }).catch(e => {
+    console.log('no such user');
+  })
+  // console.log(username,password);
+  //  allow users to be automatically logged in
+  //  props.onLoggedIn(username); 
  
-}
+};
  return(
    <Container className="login-view" fluid="true">
    <Form>
