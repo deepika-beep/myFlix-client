e0b0a0ead3e0f190174de33eeeefb92096be4b90
Deepika <75814956+deepika-beep@users.noreply.github.com>
2021-07-05 22:16:05 +0200

    added route to dir and genre views

--- src/component/movie-view/movie-view.jsx
+++ src/component/movie-view/movie-view.jsx
@@ -31,7 +31,15 @@ export class MovieView extends React.Component{
       <span className = 'label'>Description:</span>
       <span className = 'value'>{movieData.Description}</span>
       </div>
-     
+       {/* add “Director” and “Genre” buttons in the movie view and route to the director and genre views   */}
+     <Link to ={`/director/${movie.Director.Name}`}>
+     <button variant='link'>Director</button>
+     </Link>
+    <Link to ={`/genre/${movie.Genre.Name}`}>
+      <button variant='link'>Genre</button>
+
+    </Link>
+    {/* history.goBack() has been called, which means whenever you click on the back button  in “movie-view.jsx,” it will eventually call goBack() function in main-view */}
       <button variant ='link' onClick = {() => {onBackClick(null);}}>Back</button>
       </div>
       </Jumbotron>
