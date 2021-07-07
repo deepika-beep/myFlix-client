edb995819ce5e20eb2bff59dfc14b2946b9766ed
Deepika <75814956+deepika-beep@users.noreply.github.com>
2021-07-07 15:31:13 +0200

    Update registration-view.jsx

--- src/component/registration-view/registration-view.jsx
+++ src/component/registration-view/registration-view.jsx
@@ -1,16 +1,75 @@
 import React, { useState } from "react";
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import PropTypes from 'prop-types';
+import {Link} from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+import Navbar from "react-bootstrap/Navbar";
 import Form from 'react-bootstrap/Form';
 import axios from "axios";
+import './registration-view.scss';
 export function RegistrationView(props) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
   const [birthday, setBirthday] = useState("");
+// Validating states
+  const [validateUser,setValidateUser] = useState('');
+  const [validatePassword,setValidatePassword] = useState('');
+  const [validateEmail,setValidateEmail] = useState('');
+  const [validateDate,setValidateDate] = useState('');
+  const [warning, setWarning] = useState('');
 
+  // Validating username
+  const validateUsername =(e)=>{
+    if(e.target.value.length > 0 && e.target.value.length <5){
+      setValidateUser('Username must be longer than 5 characters');
+    }
+    else{
+      setValidateUser('User');
+    }
+    if(!e.currentTarget.value.match(/^[0-9a-zA-Z]+$/) && e.target.value.length >0){
+      setValidateUser('only alphanumeric charecters allowed');
+    }
+  }
+  // Validating pASSWORD
+  const validatePwd =(e)=>{
+    if(e.target.value.length >0 && e.target.value.length<8){
+      setValidatePassword('Password must be longer than 8 characters');
+    }else
+    {
+      setValidatePassword('');
+    }
+  }
+  // Validating email
+  const validateMail =(e)=>{
+    if(!e.target.value.match(/\S+@\S+\.\S+/) && e.target.value.length > 0){
+      setValidateEmail('Incorrect Email');
+    }else{
+      setValidateEmail('');
+    }
+  }
+  // validating date
+  const validateBirthday = (e) => {
+    if(!e.target.value.match(/^\d{4}-\d{2}-\d{2}$/) && e.target.value.length >0){
+      setValidateDate('Please use format (yyyy-mm-dd)');
+    }
+      else{
+        setValidateDate('');
+      } 
+    }
+  
   const handleSubmit = (e) => {
     e.preventDefault();
+    // vslidating empty inputs
+    if(username.length === 0 || password.length === 0 || email.length === 0 || birthday.length === 0){
+      setWarning('Please fill in all the fields');
+      return false
+    }
+
+    // prevent incorrect  credentials
+    if(validateUser || validatePassword || validateEmail || validateDate){
+      alert('Entered incorrect values');
+      return false
+    }
     axios.post('https://myflix-movies-api.herokuapp.com/users',{
       Username:username,
       Password:password,
@@ -25,23 +84,30 @@ export function RegistrationView(props) {
     })
     .catch(e =>{
       console.log('error registering the user');
+      if(e.response.data == `${username} or ${email} already exist`){
+        setWarning('Username or Email already exists');
+      }
     })
 
   };
 
   return (
+    <>
+     <Navbar bg="dark" variant="dark" fixed="top">
+            <Navbar.Brand href="#home">My Flix</Navbar.Brand>
+        </Navbar>
     <Form>
       <Form.Group controlId="formUsername">
      <Form.Label>Username:</Form.Label>
        <Form.Control type="text" placeholder ='enter Username' value={username}
-          onChange={(e) => setUsername(e.target.value)}
+          onChange={(e) => setUsername(e.target.value),validateUsername(e)}
         />
       </Form.Group>
 
       <Form.Group controlId="formPassword">
       <Form.Label>Create Password:</Form.Label>
        <Form.Control  type="password" placeholder ='enter password' value={password}
-          onChange={(e) => setPassword(e.target.value)}
+          onChange={(e) => setPassword(e.target.value),validatePassword(e)}
         />
         </Form.Group>
        <Form.Group controlId="formGroupEmail">
@@ -50,7 +116,7 @@ export function RegistrationView(props) {
           <Form.Control 
             type="email"
             value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            onChange={(e) => setEmail(e.target.value),validateEmail(e)}
           />
           </Form.Group>
       
@@ -59,15 +125,18 @@ export function RegistrationView(props) {
          <Form.Control 
             type="date"
             value={birthday}
-            onChange={(e) => setBirthday(e.target.value)}
+            onChange={(e) => setBirthday(e.target.value),validateBirthday(e)}
           />
           </Form.Group>
       
-      <button type="submit" onClick={handleSubmit}>
-        Submit
-      </button>
-      {/* <button onClick={() => { onBackClick(null); }}>Back</button> */}
-      <button type='button' onClick={props.toggleForms}>If you have an Account login</button>
+      <Button variant='primary' type="submit" onClick={handleSubmit}>Submit</Button>
+{/*      
+      <button type='button' onClick={props.toggleForms}>If you have an Account login</button> */}
+      <Form.Text classname='text-muted'>
+        Already have an account?Register <Link to={`/`}>here</Link>
+      </Form.Text>
+      <div className ='warning'>{warning}</div>
     </Form>
+    </>
   );
 }
