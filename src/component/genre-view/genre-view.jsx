6b3a8c00dc077023d856274077e2909ddf16c05b
Deepika <75814956+deepika-beep@users.noreply.github.com>
2021-07-07 15:32:29 +0200

    Update genre-view.jsx

--- src/component/genre-view/genre-view.jsx
+++ src/component/genre-view/genre-view.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import  Jumbotron  from 'react-bootstrap/Jumbotron';
 import Button from 'react-bootstrap/Button';
+import { Link } from "react-router-dom";
 import './genre-view.scss';
 
 export class GenreView extends React.Component {
 
   render() {
-    const { genre, onBackClick } = this.props;
+    const { genre, onBackClick} = this.props;
+    const genreMovies = movies.filter(m => m.genre.name === genre.name)
 
     return (
       <Jumbotron fluid className="GenreView">
@@ -19,9 +22,20 @@ export class GenreView extends React.Component {
             <span className="label">Description: </span>
             <span className="value">{genre.Description}</span>
           </div>
+          <div className="genre-movies">
+        
+            <Link to={`/movies/${m._id}`}>{m.title}</Link>
+            </div>
           <Button variant="link" onClick={() => { onBackClick(null); }}>Back</Button>
         </div>
       </Jumbotron>
     );
   }
 }
+GenreView.propTypes = {
+  genre: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Description: PropTypes.string.isRequired
+  }),
+   onBackClick: PropTypes.func.isRequired
+};
