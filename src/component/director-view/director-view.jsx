6b015dcf6f23c9bd1937d9a3c2e4a9fc2f4b939c
Deepika <75814956+deepika-beep@users.noreply.github.com>
2021-07-20 00:43:58 +0200

    Update director-view.jsx

--- src/component/director-view/director-view.jsx
+++ src/component/director-view/director-view.jsx
@@ -3,13 +3,19 @@ import Jumbotron  from 'react-bootstrap/Jumbotron';
 import Button from 'react-bootstrap/Button';
 import PropTypes from 'prop-types';
 import { Link } from "react-router-dom";
+import { connect } from 'react-redux';
 import './director-view.scss';
+// get movielist from  store as prop
+const mapStateToProps = state =>{
+  const{movies} = state;
+  return{movies}
+}
+export class DirectorView extends React.Component{
 
-export class DirectorView extends React.Component {
-
-  render() {
-    const { director, onBackClick } = this.props;
-
+ render() {
+    const { director,movies,onBackClick } = this.props;
+    // const directorsMovies = movies.filter(m => m.director.name === director.name)
+   
     return (
       <Jumbotron fluid className='director'>
         <div className="director-view">
@@ -26,7 +32,7 @@ export class DirectorView extends React.Component {
             <span className="value">{director.Birth}</span>
           </div>
           <div className="director-movies">
-                     <Link to={`/movies/${m._id}`}>{m.title}</Link>
+               <Link to={`/movies/${m._id}`}>{m.title}</Link>
             </div> 
           <Button variant="link" onClick={() => { onBackClick(null); }}>Back</Button>
         </div>
@@ -34,7 +40,6 @@ export class DirectorView extends React.Component {
     );
   }
 }
-
 DirectorView.propTypes = {
   director: PropTypes.shape({
     Name: PropTypes.string.isRequired,
@@ -42,5 +47,7 @@ DirectorView.propTypes = {
     Birth: PropTypes.string.isRequired
    
   }),
-  onBackClick: PropTypes.func.isRequired
+  onBackClick: PropTypes.func.isRequired,
+ 
 };
+export default connect(mapStateToProps)(DirectorView)
