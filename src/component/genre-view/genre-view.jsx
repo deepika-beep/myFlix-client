85ed823498ecd11d64ab26232231fde2d469f58b
Deepika <75814956+deepika-beep@users.noreply.github.com>
2021-08-23 16:15:57 +0200

    Create genre-view.jsx

---
+++ src/component/genre-view/genre-view.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Button, Row, Col, Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import "./genre-view.scss"
+// Getting the movie list from store as prop
+const mapStateToProps = state => {
+  const { movies } = state;
+  return { movies };
+};
+function GenreView(props) {
+
+  const { genre, clickBack, movies } = props;
+
+   const genresMovies = movies.filter(m => m.Genre.Name === genre.Name)
+console.log(genresMovies,"movies")
+
+  return (
+    <div className="director-view">
+      <h2> {genre.Name} </h2>
+      <p> {genre.Description} </p>
+
+
+      <div className="genre-movies">
+        <small>Movies belonging to this genre:</small>
+        {genresMovies.map((m, i) => <p key={m._id}> <Link to={`/movies/${m._id}`}>{m.Title}</Link> </p>)}
+      </div> <hr />
+      <Button variant="link" onClick={() => { clickBack(); }}>Back</Button>
+    </div>
+  )
+}
+
+GenreView.propTypes = {
+  genre: PropTypes.shape({
+  Name: PropTypes.string.isRequired,
+  Description: PropTypes.string.isRequired
+  }).isRequired,
+  clickBack: PropTypes.func.isRequired,
+  movies: PropTypes.array.isRequired
+}
+
+export default connect(mapStateToProps)(GenreView)
