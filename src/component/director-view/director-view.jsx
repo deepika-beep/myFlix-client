58319f5d2d6c554e2ccd6127b929a510737f1e45
Deepika <75814956+deepika-beep@users.noreply.github.com>
2021-08-23 16:16:06 +0200

    Create director-view.jsx

---
+++ src/component/director-view/director-view.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Button, Row, Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import {connect} from 'react-redux';
+
+import './director-view.scss';
+// Getting the movie list from store as prop
+const mapStateToProps = state => {
+    const {movies} = state;
+    return {movies};
+};
+function DirectorView({clickBack, Director,movies}) {
+
+    const directorsMovies = movies.filter(m => m.Director.Name === Director.Name)
+    return (
+        <div className="director-view">
+            <h2> {Director.Name} </h2>
+            <p> {Director.Bio} </p>
+
+            <div>
+                <small>Birth: </small>
+                <p>{Director.Birth.slice(0, 10)}</p>
+            </div>
+
+            <div className="director-movies">
+                    <small>Movies belonging to this director:</small>
+                    {directorsMovies.map((m, i) => <p key={m._id}> <Link to={`/movies/${m._id}`}>{m.Title}</Link> </p> )}
+            </div> <hr />
+            <Button variant="link"  onClick={() => { clickBack(); }}>Back</Button>
+        </div>
+    )
+}
+
+DirectorView.propTypes = {
+  Director: PropTypes.shape({
+    Name: PropTypes.string.isRequired,
+    Bio: PropTypes.string.isRequired,
+    Birth: PropTypes.string.isRequired
+  }).isRequired,
+  clickBack: PropTypes.func.isRequired
+};
+export default connect(mapStateToProps)(DirectorView)
